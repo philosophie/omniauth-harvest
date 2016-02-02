9c213297de7b5cb292b464e4d5cb67c3e0ccb21c
Nick Giancola <nick@gophilosophie.com>
2016-02-01 17:02:07 -0800

    Update strategy to pull in user info including uid

--- lib/omniauth/strategies/harvest.rb
+++ lib/omniauth/strategies/harvest.rb
@@ -4,32 +4,19 @@ module OmniAuth
   module Strategies
     class Harvest < OmniAuth::Strategies::OAuth2
       option :client_options, {
-        :site => 'https://api.harvestapp.com',
-        :authorize_url => '/oauth2/authorize',
-        :token_url => '/oauth2/token'
+        site: 'https://api.harvestapp.com',
+        authorize_url: '/oauth2/authorize',
+        token_url: '/oauth2/token'
       }
-      
-      def authorize_params
-        super.tap do |params|
-          params[:response_type] = "code"
-          params[:client_id] = client.id
-          params[:redirect_uri] ||= callback_url
-        end
-      end
 
-      def request_phase
-        super
-      end
+      uid { user_info['user']['id'] }
 
-      def build_access_token
-        token_params = {
-          :code => request.params['code'],
-          :redirect_uri => callback_url,
-          :client_id => client.id,
-          :client_secret => client.secret,
-          :grant_type => 'authorization_code'
-        }
-        client.get_token(token_params)
+      info { user_info }
+
+      private
+
+      def user_info
+        @user_info ||= access_token.get('/account/who_am_i.json').parsed
       end
     end
   end
